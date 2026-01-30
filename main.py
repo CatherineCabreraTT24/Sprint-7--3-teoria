c2736c57a05bebef554b81cfe1fb45d9a3c96194
CatherineCabreraTT24 <catherine.eliana@tripleten-team.com>
2026-01-29 19:58:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import seaborn as sns
-from eda_utils.stats import basic_summary, missing_values
-from eda_utils.plots import plot_histogram
+from eda_utils.stats import DataSummary
+from eda_utils.plots import HistogramPlotter
 
 
 def main():
     df = sns.load_dataset("penguins")
 
+    # Resúmenes
+    summary = DataSummary(df)
+
     print("Resumen estadístico:")
-    print(basic_summary(df))
+    print(summary.basic_summary())
 
     print("\nValores faltantes:")
-    print(missing_values(df))
+    print(summary.missing_values())
 
-    plot_histogram(df, "bill_length_mm")
+    # Gráficas
+    plotter = HistogramPlotter(df)
+    plotter.plot("bill_length_mm")
 
 
 if __name__ == "__main__":
